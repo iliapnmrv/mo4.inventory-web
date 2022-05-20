d1406935c7a9abc2fa63e4083b70da9a8a721628
iliapnmrv <iliapnmrvv@gmail.com>
2022-05-20 15:57:55 +0300

    storage fix

--- client/src/components/Item/Item.js
+++ client/src/components/Item/Item.js
@@ -4,7 +4,7 @@ import "./Item.css";
 
 export default function Item({ openModal, data, showPlaces }) {
   const { sredstva, types, storages } = useSelector((state) => state.info);
-  console.log(data);
+
   return (
     <>
       <tr data-id={data?.qr} key={data?.qr} onClick={() => openModal(data?.qr)}>
@@ -13,7 +13,12 @@ export default function Item({ openModal, data, showPlaces }) {
         <td>{types[data.type - 1]?.label}</td>
         <td>{data?.month}</td>
         <td>{data?.year}</td>
-        <td>{showPlaces ? storages[data.storage - 1]?.label : data?.name}</td>
+        <td>
+          {showPlaces
+            ? storages.filter((storage) => storage.value === data.storage)?.[0]
+                ?.label
+            : data?.name}
+        </td>
         <td>{data?.model}</td>
         <td>{data?.sernom}</td>
       </tr>
