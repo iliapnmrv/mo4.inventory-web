be141be84c6617559cde41e9ba0670daf9deeb26
iliapnmrv <iliapnmrvv@gmail.com>
2022-06-14 11:22:53 +0300

    gitignore cache fix

--- README.md
+++
@@ -1,1 +0,0 @@
-# inventory made for web
--- server/constants/constants.js
+++
@@ -1,8 +0,0 @@
-export const SERVER = "http://mo4-it5:8000/"
-
-export const catalogsNames = {
-    storage: "storages",
-    owner: "owners",
-    status: "statuses",
-    person: "persons",
-}
