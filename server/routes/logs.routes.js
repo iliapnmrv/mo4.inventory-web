8318dced47c053ce301ee43456310f888487b33d
iliapnmrv <iliapnmrvv@gmail.com>
2022-08-02 09:30:34 +0300

    gitignore fix

--- server/routes/logs.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-const router = Router()
-
-import logsController from "../controller/logs.controller.js";
-
-router.post('/', logsController.createLog)
-router.get('/:id', logsController.getOne)
-
-export default router
